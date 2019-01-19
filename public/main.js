7110f77a08d9d01701631c4e69a378aace06f683
GuoLei Sun <sunguolei@100tal.com>
2019-01-19 21:51:16 +0800

    update the welcome message and title

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to Ying Chat – ";
     log(message, {
       prepend: true
     });
